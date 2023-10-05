1b468c3a9c8771725388e51085b984a55c026399
Lollo <41307169+LollosaurusRex@users.noreply.github.com>
2023-10-05 10:39:12 -0500

    Added documentation to TuneCreator and realigned note button

--- Project/Scenes/TuneCreator/TuneCreator.gd
+++ Project/Scenes/TuneCreator/TuneCreator.gd
@@ -1,11 +1,21 @@
 extends Node2D
 class_name TuneCreator
 
+# The tune creator is an object that handles, in our main scene, the generation of notes
+# 	- Must be given a Path2D in the inspector (this Path2D must have its PathFollow2D).
+#	- Then, the TuneCreator be given the scene for the note.
+#
+# For n notes, the tune creator divides the path into equal chunks
+# A note is created, then added as a child of the TuneCreator.
+# 	NOTE: the note's position is edited with *global* position
+
 @export var path : Path2D
 @onready var pathFollower = path.get_child(0)
-@export var dummyNote : PackedScene
+@export var dummyNote : PackedScene # This is a dummy note of a dog sprite.
+@export var note : PackedScene		# Real note. Button functionality and sound
 
 @export var numNotes : int = 5
+@export var debug : bool = true
 
 var listOfNotes : Array
 
@@ -27,6 +37,10 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	pass
+
+func generate() -> void:
+	
 	pass
 
 func cleanup() -> void:
