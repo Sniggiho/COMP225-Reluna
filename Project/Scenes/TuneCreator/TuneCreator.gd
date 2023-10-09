b96ecde044a39086795b84e2a6cbbf86a0e04773
Sniggiho <rohiggin@macalester.edu>
2023-10-09 18:14:45 -0500

    tune creator can create set of notes for any key

--- Project/Scenes/TuneCreator/TuneCreator.gd
+++ Project/Scenes/TuneCreator/TuneCreator.gd
@@ -21,16 +21,12 @@ class_name TuneCreator
 var listOfNotes : Array
 
 # Pregenerated list of all possible notes 
-var allPossibleNotes : Array
+var possibleNotes : Array
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	allPossibleNotes = _createPossibleNoteArray(3,5)
-
+	possibleNotes = _createNoteArrayInKey(4,3,5, false)
 	generate()
-
-
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -44,7 +40,7 @@ func generate() -> void:
 		pathFollower.progress_ratio += dx
 		var note = noteScene.instantiate()
 		note.setDetuneCents(randi_range(-50,50))
-		note.setNoteByName(allPossibleNotes[randi() % allPossibleNotes.size()])
+		note.setNoteByName(possibleNotes[randi() % possibleNotes.size()])
 		
 		add_child(note)
 		listOfNotes.append(note)
@@ -55,7 +51,7 @@ func cleanup() -> void:
 	for note in listOfNotes:
 		note.queue_free()
 
-func _createPossibleNoteArray(lowOct, highOct)-> Array:
+func _createNoteArrayChromatic(lowOct, highOct)-> Array:
 	""" This currently just generates all the possible note names between the two given octaves.
 	Later, this should be able to generate specific keys as note sets, and give more control in the exact range created,
 	the idea being that the user will someday adjust these parameters"""
@@ -67,3 +63,30 @@ func _createPossibleNoteArray(lowOct, highOct)-> Array:
 				allNotes.append(note+"-"+str(oct))
 	return allNotes
 	
+func _createNoteArrayInKey(accidentals, lowOct, highOct, bySharps=true) -> Array:
+	""" Creates an array containing the note names for a mode with the specified number of sharps"""
+	# sharps order is: F – C – G – D – A – E – B
+	# flats order is: B - E - A - D - G - C - F
+	
+	var sharps = ["f-", "c-", "g-", "d-", "a-", "e-", "b-"]
+	var flats = ["a-", "d-", "g-", "c-", "f-", "b-", "e-"]
+	
+	var notesInKey = []
+	var allNotes = []
+	
+	if bySharps:
+		for i in range(accidentals):
+			notesInKey.append(sharps[i])
+		for i in range(accidentals, 7):
+			notesInKey.append(sharps[i].replace("-",""))
+	else:
+		for i in range(accidentals):
+			notesInKey.append(flats[i])
+		for i in range(accidentals, 7):
+			notesInKey.append(flats[i].replace("-",""))
+	
+	for oct in range(lowOct,highOct+1):
+		for note in notesInKey:
+			allNotes.append(note+str(oct))
+	
+	return allNotes
