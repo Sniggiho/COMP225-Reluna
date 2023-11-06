dd93142e03d90d9043001034ce2e3a3fcc4baad4
Lollo <41307169+LollosaurusRex@users.noreply.github.com>
2023-11-06 12:59:36 -0600

    min and max detune cents sliders added

---
+++ Project/Scenes/FreeplaySettingsMenu/MaxDetuneCents.gd
@@ -0,0 +1,17 @@
+extends VSlider
+
+var text : RichTextLabel = RichTextLabel.new()
+
+func _ready() -> void:
+	text.text = str(value)
+	add_child(text)
+	text.size = Vector2(100, 100)
+	text.mouse_filter = Control.MOUSE_FILTER_IGNORE
+	text.position = Vector2(0, -30)
+	
+	GLevelData.maxDetuneCents = value
+
+
+func _on_value_changed(passedValue):
+	text.text = str(passedValue)
+	GLevelData.maxDetuneCents = passedValue
