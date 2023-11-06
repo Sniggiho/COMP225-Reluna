e27ef586e3ee2c53b9751a4981b9d2b5c059cd2e
Sniggiho <rohiggin@macalester.edu>
2023-11-06 14:53:37 -0600

    generate possible notes for freeplay menu

--- Project/Scenes/FreeplaySettingsMenu/FreeplaySettingsMenu.gd
+++ Project/Scenes/FreeplaySettingsMenu/FreeplaySettingsMenu.gd
@@ -1,2 +1,13 @@
 extends Node2D
 class_name FreeplaySettingsMenu
+
+func generatePossibleNotes() -> Array:
+	var noteOrder = ["c","c-","d","d-","e","f","f-","g","g-","a","a-","b"]
+	var possibleNotes = []
+	for i in range(3,6):
+		for n in noteOrder:
+			var noteName = n + str(i)
+			possibleNotes.append(noteName)
+	
+	possibleNotes.append("c6")
+	return possibleNotes
