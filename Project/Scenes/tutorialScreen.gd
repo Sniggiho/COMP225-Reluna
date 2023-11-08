da332cd8f0067fb50a7c5d0799249eb0cef85879
Sniggiho <rohiggin@macalester.edu>
2023-11-08 10:34:16 -0600

    second tutorial screen

--- Project/Scenes/tutorialScreen.gd
+++ Project/Scenes/tutorialScreen.gd
@@ -4,3 +4,5 @@ func _on_return_button_pressed():
 	SceneTransition.change_scene("res://Scenes/menuScreen.tscn")
 
 
+func _on_next_button_pressed():
+	SceneTransition.change_scene("res://Scenes/tutorialScreen2.tscn")
---
+++ Project/Scenes/tutorialScreen2.gd
@@ -0,0 +1,4 @@
+extends Node2D
+
+func _on_return_button_pressed():
+	SceneTransition.change_scene("res://Scenes/tutorialScreen.tscn")
