2dba307fb52bd88d4f51b56564c73f2d4915b78e
Lollo <41307169+LollosaurusRex@users.noreply.github.com>
2023-11-08 15:20:59 -0600

    Made return button grab focus

--- Project/Scenes/FreeplaySettingsMenu/MaxNote.gd
+++ Project/Scenes/FreeplaySettingsMenu/MaxNote.gd
@@ -5,7 +5,7 @@ var text : RichTextLabel = RichTextLabel.new()
 var notes : Array = ["c3", "c-3", "d3", "d-3", "e3", "f3", "f-3", "g3", "g-3", "a3", "a-3", "b3", 
 					 "c4", "c-4", "d4", "d-4", "e4", "f4", "f-4", "g4", "g-4", "a4", "a-4", "b4", 
 					 "c5", "c-5", "d5", "d-5", "e5", "f5", "f-5", "g5", "g-5", "a5", "a-5", "b5", 
-					 "c6"]
+					 "c6", "c6"]
 					
 var noteMinIndex : int = 9
 var noteMaxIndex : int = 36
@@ -25,7 +25,7 @@ func _ready():
 	text.size = Vector2(100, 100)
 	add_child(text)
 	
-	GLevelData.highestNote = notes[value]
+	GLevelData.highestNote = notes[value+1]
 	
 #	var numTicks : int = noteMaxIndex - noteMinIndex + 1
 #	tick_count = numTicks
@@ -36,7 +36,7 @@ func _on_value_changed(passedValue):
 	if passedValue < currNoteMin:
 		value = currNoteMin
 	text.text = notes[value]
-	GLevelData.highestNote = notes[value]
+	GLevelData.highestNote = notes[value+1]
 
 
 # PASSED FROM MIN NOTE
