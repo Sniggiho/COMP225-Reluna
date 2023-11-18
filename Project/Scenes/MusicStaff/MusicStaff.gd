16822f58c36ef21d930dc9e31938562e6359541c
Sniggiho <rohiggin@macalester.edu>
2023-11-18 15:59:44 -0600

    start on algorithmic accidental placement (broken)

--- Project/Scenes/MusicStaff/MusicStaff.gd
+++ Project/Scenes/MusicStaff/MusicStaff.gd
@@ -31,16 +31,27 @@ func _ready():
 	screenY = get_viewport_rect().size.y
 	startX = 0
 	startY = 0
+	var lastAccidental : Sprite2D
 	
 	if not GLevelData.bySharps:
 		for i in range(1,GLevelData.numAccidentals+1):
-			get_node("sharp" + str(i)).show()  
+			get_node("sharp" + str(i)).show()
+		lastAccidental = get_node("sharp" + str(GLevelData.numAccidentals))
 	else:
 		for i in range(1,GLevelData.numAccidentals+1):
 			get_node("flat" + str(i)).show()
+		print( get_node("flat" + str(GLevelData.numAccidentals)))
+		lastAccidental = get_node("flat" + str(GLevelData.numAccidentals))
 	
-	var buffer = staffLength*screenX*0.05
-	self.get_child(1).get_curve().set_point_position(0,Vector2(buffer+45*GLevelData.numAccidentals,0))
+	var xBuffer = 0
+	if GLevelData.numAccidentals == 0:
+		xBuffer = lastAccidental.get_rect().position.x
+	else:
+		xBuffer = lastAccidental.get_rect().end.x
+
+	var accidentalWidth = lastAccidental.get_rect().end.x - lastAccidental.get_rect().position.x
+	print("xbuffer is", xBuffer, "accidenbtal width is", accidentalWidth)
+	self.get_child(1).get_curve().set_point_position(0,Vector2(xBuffer+accidentalWidth*GLevelData.numAccidentals,0))
 
 
 func _draw():
