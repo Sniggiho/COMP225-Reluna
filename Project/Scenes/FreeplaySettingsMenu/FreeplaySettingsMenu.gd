a2d6d0a00392a0e027fdfb84a3249f1f58c8e7d2
Sniggiho <rohiggin@macalester.edu>
2023-12-15 21:34:38 -0600

    tiny cosmetic changes

--- Project/Scenes/FreeplaySettingsMenu/FreeplaySettingsMenu.gd
+++ Project/Scenes/FreeplaySettingsMenu/FreeplaySettingsMenu.gd
@@ -5,7 +5,7 @@ func _ready():
 	# What a fucking mess
 	$ForwardButton.focus_neighbor_bottom = $ForwardButton.get_path_to($"CenterContainer/VBoxContainer/CenterContainer/HBoxContainer/Column B/ColumnB/NumNotes")
 	
-	$ForwardButton.grab_focus()
+	#$ForwardButton.grab_focus()
 	
 #	$"CenterContainer/VBoxContainer/CenterContainer/HBoxContainer/Column B/ColumnB/NumNotes".focus_neighbor_top = $"CenterContainer/VBoxContainer/CenterContainer/HBoxContainer/Column B/ColumnB/NumNotes".get_path_to($"ForwardButton")
 	
@@ -15,6 +15,10 @@ func _ready():
 func _input(event):
 	if event.is_action_pressed("ui_cancel"):
 		_goto_main_menu()
+	if event.is_action_pressed('ui_left') or event.is_action_pressed('ui_right') or event.is_action_pressed('ui_down') or event.is_action_pressed('ui_up'):
+		$ForwardButton.grab_focus()
+		
+
 
 
 func _on_button_pressed():
